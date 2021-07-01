7253880ed8c8b08a1bab2c8016732d7c60efc353
Chris <chrisrlowden@gmail.com>
2021-06-30 23:18:37 -0400

    changed small breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'md': '876px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  extend: {},
   },
   variants: {},
   plugins: [],
